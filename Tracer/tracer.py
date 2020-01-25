0b85b984551d111ec73583ae4b42e7126b499dec
0xSmiley <nunolopess@protonmail.com>
2020-01-25 12:52:20 +0000

    test

--- Tracer/tracer.py
+++ Tracer/tracer.py
@@ -10,12 +10,12 @@ def load_modules():
 def main():
     prog=load_modules()
     #print(prog)
-    prog2 = """
+    prog2 = "
 int clone(struct pt_regs *ctx) {
     bpf_trace_printk("Hello, Clone!\\n");
     return 0;
 }
-        """
+        "
     #print(prog2)
     b = BPF(text=prog)
     b.attach_kprobe(event=b.get_syscall_fnname("clone"), fn_name="clone")
