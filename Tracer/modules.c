0aefc877180961f978fd3246a7cbdba6bc3604a0
0xSmiley <nunolopess@protonmail.com>
2020-01-25 13:04:18 +0000

    anonymous func

--- Tracer/modules.c
+++ Tracer/modules.c
@@ -1,4 +1,6 @@
-int clone(struct pt_regs *ctx) {
-    bpf_trace_printk("Hello, Clone!\n");
-    return 0;
+#define TRACE_ENT_SYSCALL(name)                                         \
+int syscall__##name(struct pt_regs *ctx)                                \
+{                                                                       \
+    bpf_trace_printk("Hello, Clone!\n");                                \
+    return 0                                                            \
 }
