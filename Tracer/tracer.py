111efc43d52a389366ba76acdb8da3e5b2fdb6ed
0xSmiley <nunolopess@protonmail.com>
2020-02-25 15:17:37 +0000

    Adding Captures Cntrl+C aproach

--- Tracer/tracer.py
+++ Tracer/tracer.py
@@ -3,7 +3,6 @@ import socket
 import os
 import seccompGenerator
 import time
-import docker
 from threading import Timer
 
 
@@ -40,9 +39,6 @@ def main():
     hostnameContainer = socket.gethostname()
     hostnameHost= os.environ['HOST_HOSTNAME']
     
-    
-    client = docker.from_env()
-    containerList=client.containers.list()
     print("Tracing")
     
     while 1:
@@ -51,7 +47,12 @@ def main():
             (task, pid, cpu, flags, ts, msg) = b.trace_fields()
         
         except KeyboardInterrupt:
-            exit()
+            for utsTmp in fileDesc:
+                fdTmp=fileDesc[utsTmp]
+                fdTmp.close()
+                seccompGenerator.EbpfMode(utsTmp)
+                print("Traced "+utsTmp)
+            break
         
         
         msg=msg.decode("utf-8") 
@@ -72,30 +73,6 @@ def main():
                 #print("%f;%s;%s;%s" % (ts, task, uts, syscall))
             except Exception:
                 print("Error on "+uts+ " "+ task+ " "+syscall)
-
-            currentContainerList=client.containers.list()
-            if len(containerList)<len(currentContainerList):
-                containerList=currentContainerList
-                #print(containerList)
-            elif len(containerList)>len(currentContainerList):
-                #print(containerList,currentContainerList)
-                t=Timer(10,stopTraceInit,[containerList,currentContainerList])
-                #t = threading.Thread(target=stopTraceInit, args=[containerList,currentContainerList])
-                t.start()
-                containerList=currentContainerList
-
-def stopTraceInit(containerList,currentContainerList):
-    currentContainerList=set(currentContainerList)
-    diff = [x for x in containerList if x not in currentContainerList]
-
-    for container in diff:
-        fullUts=container.id
-        uts=fullUts[:12]
-        fd=fileDesc[uts]
-        seccompGenerator.EbpfMode(uts)
-        fd.close()
-        print("Traced "+uts)
-
         
 
 if __name__== "__main__":
