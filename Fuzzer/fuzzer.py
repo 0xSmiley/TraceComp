089164da256804061ca016689ca5eaaf527cddf4
0xSmiley <nunolopess@protonmail.com>
2020-03-04 17:18:06 +0000

    Radamsa container

--- Fuzzer/fuzzer.py
+++ Fuzzer/fuzzer.py
@@ -1,11 +1,23 @@
 import json
-import os
+import docker
+import os 
+
+client = docker.from_env()
 
 with open('conf.json') as json_file:
     data = json.load(json_file)
     for line in data['fuzz-url']:
-        contaienr=line['container']
+        container=line['container']
         args=line['command']
-        os.system("docker run --rm "+ contaienr+" "+ args)
+        if 'inputs' in line:
+            
+            fullPath=line['inputs']
+            temp = fullPath.split('/') 
+            res = '/'.join(temp[:len(temp)-1]), '/'.join(temp[len(temp)-1:])
+            path=res[0]
+            mutateFile=res[1]
+
+            os.system('docker run --rm -v '+path+':/Inputs nunolopes97/radamsa -r /Inputs/'+mutateFile+' -n 10 -o /Inputs/out%n.txt')
+        #os.system("docker run --rm "+ container+" "+ args)
         
         
