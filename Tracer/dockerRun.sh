4b6bc2187bfc50390a1f0eee848566fada4ac4cd
0xSmiley <nunolopess@protonmail.com>
2020-04-30 11:00:39 +0100

    Updates

--- Tracer/dockerRun.sh
+++ Tracer/dockerRun.sh
@@ -5,7 +5,7 @@ docker run -it --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /etc/localtime:/etc/localtime \
   -v /sys/kernel/debug:/sys/kernel/debug \
-  -v /home/ubuntu/Desktop/Share/Runtime/Tracer:/tracer \
+  -v $PWD:/tracer \
   -e HOST_HOSTNAME=`hostname` \
   --workdir /tracer \
   --network="host" \
